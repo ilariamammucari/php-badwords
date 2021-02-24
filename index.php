bc02601f47cebaebdf1424903f087a80ea74d4f6
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-24 15:23:36 +0100

    esercizio con php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<?php 
-
+<!-- php -->
+<?php
 $linguaggio = 'PHP';
 $lunghezza = strlen($linguaggio);
 $parola = $_GET['badword'];
 $sostituzioneParola = str_replace($parola, '***', $parola);
-
-echo $parola . ' questa parola è sostituita con: ' . $sostituzioneParola;
-
 ?>
+<!-- /php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,8 +15,14 @@ echo $parola . ' questa parola è sostituita con: ' . $sostituzioneParola;
     <title>Primo esercizio in PHP</title>
 </head>
 <body>
+
     <h1>
     Benvenuti nella prima pagina creata con <?php echo $linguaggio . '.' . ' La lunghezza della variabile $linguaggio è di ' .  $lunghezza . ' caratteri.' ?>
     </h1>
+
+    <!-- php -->
+    <p><?= $parola ?> questa parola è sosituita con <?= $sostituzioneParola ?> </p>
+    <!-- /php -->
+
 </body>
 </html>
